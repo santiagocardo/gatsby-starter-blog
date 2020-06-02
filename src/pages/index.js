745c816d073f7e095c408d1a7ef3c4676297dc6b
Santiago Cardona <scardona@lean-tech.io>
2020-06-02 15:59:08 -0500

    📝 MDX Blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import Layout from '../components/layout'
 
-export default () => (
-  <Layout>
-    <h1>Home</h1>
-    <p>Hello Girardota!</p>
-    <Link to="/about/">Learn about me &rarr;</Link>
-  </Layout>
-)
+import Layout from '../components/layout'
+import usePosts from '../hooks/use-posts'
+import PostPreview from '../components/postPreview'
+
+export default () => {
+  const posts = usePosts()
+
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>Hello Girardota!</p>
+      <Link to="/about/">Learn about me &rarr;</Link>
+
+      <h2>Read my blog</h2>
+      {
+        posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))
+      }
+    </Layout>
+  )
+}
