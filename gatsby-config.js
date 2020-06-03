4aad1e788e2238eeb2734bbd99ab5e80ce5ace5a
Santiago Cardona <scardona@lean-tech.io>
2020-06-03 17:42:29 -0500

    🕵  Analyze and Deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,15 @@ module.exports = {
       options: {
         username: 'gatsbyjs'
       }
+    },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static'
+      }
     }
   ]
 }
