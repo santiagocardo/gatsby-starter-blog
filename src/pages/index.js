ddb9322f9b92ae4b4c1554aa0817aa0a063b6223
Santiago Cardona <scardona@lean-tech.io>
2020-06-02 19:34:25 -0500

    🖼  Images with Gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,24 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import usePosts from '../hooks/use-posts'
 import PostPreview from '../components/postPreview'
+import Hero from '../components/hero'
 
 export default () => {
   const posts = usePosts()
 
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Hello Girardota!</p>
-      <Link to="/about/">Learn about me &rarr;</Link>
-
-      <h2>Read my blog</h2>
-      {
-        posts.map(post => (
-          <PostPreview key={post.slug} post={post} />
-        ))
-      }
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read my blog</h2>
+        {
+          posts.map(post => (
+            <PostPreview key={post.slug} post={post} />
+          ))
+        }
+      </Layout>
+    </>
   )
 }
